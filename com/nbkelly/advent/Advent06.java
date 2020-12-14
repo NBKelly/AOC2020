c1d521990e2b024127125b5a5b93a4d9388f765a
NB Kelly <N.B.Kelly@protonmail.com>
2020-12-14 22:50:35 +1300

    Added AOC problem 06

---
+++ com/nbkelly/advent/Advent06.java
@@ -0,0 +1,154 @@
+package com.nbkelly.advent;
+import com.nbkelly.helper.DebugLogger;
+import com.nbkelly.helper.ConceptHelper;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Advent06 extends ConceptHelper {
+    //BE AWARE OF:
+    //  DEBUG, IGNORE UNCLEAN, TIMER, DEBUG_TIME_MAGNITUDE
+    //  the timer and debug functions make use of these
+
+    //BE AWARE OF:
+    //  PAGE_ENABLED
+    //  PAGE_ENABLED disables real time input, but allows for arbitrary navigation of input stream (by line)
+    //  making use of the Reset and Page functions, ie
+    //  Page()          -> int page
+    //  Reset(int page) -> ()
+    // to pass a logger to another class:
+    //   getDebugLogger()  :  seeDebugLogger.java
+
+    // Input:
+    //   * NOTE: checking must be explicit (existence is assumed)
+    
+    //   hasNextInt()        -> boolean, input contains another int
+    //   hasNextBigInteger() -> boolean, same as above but bigint
+    //   hasNextLine()       -> boolean, same as above but String (full line)
+    //   hasNext()           -> boolean, same as above but String (token)
+    //   hasNextDouble()     -> boolean, same as above but double
+    //   hasNextBigDecimal() -> boolean, same as above but bigdecimal
+    //   nextInt()           -> integer, gets next int from stream
+    //   nextBigInteger()    -> bigint,  gets next bigint from stream
+    //   nextLine()          -> String,  gets all text from stream up to next line break
+    //   next()              -> string, gets next token from the stream
+    //   nextBigInteger()    -> bigint,  gets next bigint from stream
+    //   nextBigDecimal()    -> bigdec,  gets next bigdecimal from stream
+    
+    // To print:
+    //   Errors: ERR(x)       -> uses stderr, only enabled with -se
+    //   Line:   println(x)
+    //   Block:  print(x)     -> inserts space after integer argument
+    //   Format: printf(x,[]) -> standard printf
+    //   Debug:  DEBUG(x)     -> uses stderr, only enabled with -d
+    //   Debug:  DEBUGF(x,[]) -> uses stderr, only enabled with -d
+    //   Timer:  TEBUG(x)     -> uses stderr, enabled with -d/-t
+    //   Timer:  TEBUGF(x,[]) -> uses stderr, enabled with -d/-t
+    //
+    // These take input of arbitrary object, or integer
+    // V(object) = object.toString();
+
+    // Timer:
+    // * start()             -> gets time, returns timer too
+    //   split()             -> split(null)
+    // * split(Str)          -> prints split time, and also reason
+    // * total(Str, bool)    -> prints total time, reason, bool = reset
+    //   total()             -> total(null, false)
+    //   total(Str)          -> total(Str, false)
+    //   total(bool)         -> total(null, bool)
+    //   reset()             -> resets timer
+    
+    // GCD, LCM:
+    //   Single pairs, or lists of numbers are supported <>, []
+    
+    //this is basically your main method
+    public void solveProblem() throws Exception {
+	Timer t = new Timer().start();
+
+	int partTwo_total = 0;
+	int partOne_total = 0;
+        while(hasNextLine()) {	    
+	    TreeSet<Character> partTwo = new TreeSet<>();
+	    TreeSet<Character> partOne = new TreeSet<>();
+
+	    boolean first = true;
+	    String line = "";
+	    //while we have new lines. When we get an empty line, we break
+	    while(hasNextLine() && !(line = nextLine()).equals("empty")) {
+		TreeSet<Character> charset = new TreeSet<>();
+		for(int i = 0; i < line.length(); i++)
+		    charset.add(line.charAt(i));
+		if(first) {
+		    partTwo.addAll(charset);
+		    first = false;
+		} else {
+		    
+		    partTwo.retainAll(charset);	//NOTE: retainAll is intersection
+		}
+
+		partOne.addAll(charset);
+	    }
+
+	    partTwo_total += partTwo.size();
+	    partOne_total += partOne.size();
+	}
+
+	DEBUGF("PART ONE: ");
+	println(partOne_total);
+
+	DEBUGF("PART TWO: ");
+	println(partTwo_total);
+
+	t.total("Finished processing of file. ");
+    }
+        
+    //do any argument processing here
+    public boolean processArgs(String[] argv) {
+	for(int i = 0; i < argv.length; i++) {
+	    switch(argv[i]) {
+	    case "-se" : IGNORE_UNCLEAN = false; break;
+	    case "-d"  : DEBUG = true; IGNORE_UNCLEAN = false; TIMER = true; break;
+	    case "-t"  : TIMER = true; break;
+	    case "-dt" :
+		Scanner tst = null;
+		if(i + 1 < argv.length &&
+		   (tst = new Scanner(argv[i+1])).hasNextInt()) {
+		    DEBUG_TIME_MAGNITUDE = tst.nextInt();
+		    i++;
+		    break;
+		}
+		return fail();
+		
+		
+	    default :
+		return fail();
+	    }
+	}
+
+
+	//set page enabled - if paging is enabled, the entire input stream will be read in advance
+	//                   and it will be possible to arbitrarily skip between places in the
+	//                   input stream
+	PAGE_ENABLED = true;
+	
+	return true; //everything is fine
+    }
+
+    //customize your usage text here
+    private boolean fail() {
+	System.err.println("Usage: -se       = (show exceptions),\n" +
+			   "       -d        = debug mode,\n" +
+			   "       -t        = timer mode (debug lite),\n" +
+			   "       -dt <int> = set timer digits");
+	return false; //false - exit program
+    }
+    
+    public Advent06() {
+	super();
+    }
+    
+    public static void main(String[] argv) {
+	new Advent06().process(argv);
+    }
+
+    
+}
