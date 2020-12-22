2b8fbfc3b53dfd3d76d310dda0bf57ae1bff27bd
NB Kelly <N.B.Kelly@protonmail.com>
2020-12-22 17:11:17 +1300

    needed for util 2sum

---
+++ com/nbkelly/helper/Pair.java
@@ -0,0 +1,16 @@
+package com.nbkelly.helper;
+
+/**
+ * @author      NB Kelly <nbkelly @ protonmail.com>
+ * @version     1.1
+ * @since       1.1
+ */
+public final class Pair<T, U> {
+    public T X;
+    public U Y;
+
+    public Pair(T X, U Y) {
+	this.X = X;
+	this.Y = Y;
+    }
+}
